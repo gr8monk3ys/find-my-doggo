a79b2d63b7db91ca39dabfe792230f37e3acd6a2
Lorenzo <lorenzosca7@gmail.com>
2019-03-02 11:06:26 -0800

    Added stuff, changes done by Jeffrey.
    Restoring index.html

--- doggo_website/src/App.js
+++ doggo_website/src/App.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import {Container} from "semantic-ui-react";
+
+//IMPORT COMPONENTS
+import NavBar from './Components/navBar';
+import WorkExperience from "./Components/workExperience";
+
+//IMPORT CSS
+import "./Assets/css/menu.css";
+import "./Assets/css/Container.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <NavBar/>
+        <Container>
+        <WorkExperience/>
+        </Container>
+      </React.Fragment>
     );
   }
 }
