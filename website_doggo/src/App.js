d7dfa05b0bd24492d9e7b321f2b4060d89ad4932
Lorenzo <lorenzosca7@gmail.com>
2019-03-02 23:08:09 -0800

    Fuck it

--- website_doggo/src/App.js
+++ website_doggo/src/App.js
@@ -1,24 +1,25 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import {Container} from "semantic-ui-react";
 
 //IMPORT COMPONENTS
 import NavBar from './Components/navBar';
 import SubHeader from "./Components/subHeader";
+//import Container from "./Components/Container"
 
 //IMPORT CSS
 import "./Assets/css/menu.css";
 import "./Assets/css/Container.css";
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <React.Fragment>
         <NavBar/>
         <SubHeader/>
-        <Container>
-        </Container>
+        <Container/>
       </React.Fragment>
+
     );
   }
 }
