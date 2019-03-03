f832c017b33c295ed58321156bdc9288ee71d886
Lorenzo <lorenzosca7@gmail.com>
2019-03-03 03:46:46 -0800

    extra  components

---
+++ website_doggo/src/Components/Button.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Button} from "semantic-ui-react";
+
+class button extends Component {
+    render(){
+        return(
+        <button class="ui button">
+             Browse  
+        </button>
+        );
+    }
+} 
+
+export default Button
