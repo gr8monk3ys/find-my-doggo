d40082f18b9048dba46073f1970b04b19c458267
Lorenzo <lorenzosca7@gmail.com>
2019-03-03 02:12:14 -0800

    trying to push

--- website_doggo/src/App.js
+++ website_doggo/src/App.js
@@ -5,19 +5,21 @@ import {Container} from "semantic-ui-react";
 //IMPORT COMPONENTS
 import NavBar from './Components/navBar';
 import SubHeader from "./Components/subHeader";
-//import Container from "./Components/Container"
+import List from './Components/list';
 
 //IMPORT CSS
 import "./Assets/css/menu.css";
 import "./Assets/css/Container.css";
 
 class App extends React.Component {
+  
   render() {
     return (
       <React.Fragment>
         <NavBar/>
         <SubHeader/>
-        <Container/>
+        <Container/> 
+        <List />
       </React.Fragment>
 
     );
