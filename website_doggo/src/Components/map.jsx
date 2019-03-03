76ad5aa7d65b502118862fa336fa16e66ce76ed0
Lorenzo <lorenzosca7@gmail.com>
2019-03-02 20:42:21 -0800

    fix

---
+++ website_doggo/src/Components/map.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Map extends Component {
+    componentDidMount(){
+
+    }
+render(){
+    return(
+        <React.Fragment>
+            <div id="map"></div>
+        </React.Fragment>
+    );
+}
+}
+
+export default Map;
